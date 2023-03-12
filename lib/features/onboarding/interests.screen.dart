d2072a61b592431f2bb424ec324cf19937552059
jinki <jkhsky121@gmail.com>
2023-03-12 16:04:54 +0900

    tutorialScreen

--- lib/features/onboarding/interests.screen.dart
+++ lib/features/onboarding/interests.screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/tutorial_screen.dart';
 import 'package:tiktok_clone/features/onboarding/widgets/interest_button.dart';
 
 const interests = [
@@ -68,6 +69,11 @@ class _InterestsScreenState extends State<InterestsScreen> {
         _showTitle = false;
       });
     }
+  }
+
+  void _onNextTap() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const TutorialScreen()));
   }
 
   @override
@@ -144,7 +150,7 @@ class _InterestsScreenState extends State<InterestsScreen> {
                 right: Sizes.size24,
               ),
               child: CupertinoButton(
-                onPressed: () {},
+                onPressed: _onNextTap,
                 color: Theme.of(context).primaryColor,
                 child: const Text("Next"),
               )),
