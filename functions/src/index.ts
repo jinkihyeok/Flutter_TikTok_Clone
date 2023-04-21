171e914da3586ae73918b82483913870a630e69d
jinki <jkhsky121@gmail.com>
2023-04-21 16:08:35 +0900

    push notification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -44,6 +44,23 @@ export const onLikedCreated = functions.firestore
       .collection("videos")
       .doc(videoId)
       .update({ likes: admin.firestore.FieldValue.increment(1) });
+    const video = (await db.collection("videos").doc(videoId).get()).data();
+    if (video) {
+      const creatorUid = video.creatorUid;
+      const user = (await db.collection("users").doc(creatorUid).get()).data();
+      if (user) {
+        const token = user.token;
+        await admin.messaging().sendToDevice(token, {
+          data: {
+            screen: "123",
+          },
+          notification: {
+            title: "someone liked your video",
+            body: "check it out",
+          },
+        });
+      }
+    }
   });
 
 export const onLikedRemoved = functions.firestore
